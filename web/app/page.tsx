3e0a792996df6bece4807ded894c0d3db2fa98ea
PC-Ngumoha <pcngumoha@gmail.com>
2025-10-15 20:18:46 +0100

    U.I work in progress

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,5 +1,6 @@
 import { Tourney } from 'next/font/google';
 import { type JSX } from 'react';
+import { ToggleGroup, ToggleGroupItem} from '@/components/ui/toggle-group';
 
 const tourney = Tourney({
   variable: "--font-tourney",
@@ -12,11 +13,21 @@ export default function HomePage(): JSX.Element {
     <main>
       {/* Navbar */}
       <nav className='p-5 shadow'>
-        <span className={`${tourney.className} text-3xl tracking-wider text-primary`}>EppMe</span>
+        <span className={`${tourney.className} text-3xl tracking-wider text-soft-green`}>EppMe</span>
       </nav>
 
       {/* Input Form */}
-      <form action=""></form>
+      <form action="" className='bg-green-50 w-[40%] mx-auto my-8 p-4'>
+        <div className='flex justify-between items-center px-4 m-2'>
+          <h2>What&apos;s on your mind?</h2>
+
+          <ToggleGroup type='single'>
+            <ToggleGroupItem value='need'>Need</ToggleGroupItem>
+            <ToggleGroupItem value='offer'>Offer</ToggleGroupItem>
+          </ToggleGroup>
+        </div>
+        <hr className='border-2 border-gray-400'/>
+      </form>
     </main>
   )
 }
