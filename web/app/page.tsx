1b116a9393168242d3033f0d04dae802e33d387b
PC-Ngumoha <pcngumoha@gmail.com>
2025-10-15 18:46:28 +0100

    Init ShadCN

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -10,10 +10,13 @@ const tourney = Tourney({
 export default function HomePage(): JSX.Element {
   return (
     <main>
-
+      {/* Navbar */}
       <nav className='p-5 shadow'>
         <span className={`${tourney.className} text-3xl tracking-wider text-primary`}>EppMe</span>
       </nav>
+
+      {/* Input Form */}
+      <form action=""></form>
     </main>
   )
 }
